242a53e46e5a50daed04e6e81b9d2b91207d2d78
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2024-12-10 23:00:57 +0600

    Filter on AllVisa Addede

--- src/Layouts/AllVisa.jsx
+++ src/Layouts/AllVisa.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { JackInTheBox } from 'react-awesome-reveal';
 import { Link, useLoaderData } from 'react-router-dom';
-import Swal from 'sweetalert2';
 
 const AllVisa = () => {
 
     const VisaData = useLoaderData()
     const [AllVisa, setAllVisa] = useState(VisaData)
+
+    const HandleFilter = (e) => {
+        const Value = e.target.value
+        // console.log(Value)
+        const Filtered = VisaData.filter(data => data.visaType == Value)
+        // console.log(Filtered)
+        if (Value && Value != "x") {
+            setAllVisa(Filtered)
+        }
+        else {
+            setAllVisa(VisaData)
+        }
+    }
+
+
+    // { Filter ? setAllVisa(VisaData.filter(data => data.visaType == Filter)) : setAllVisa(VisaData) }
+
+    const visaTypes = ["Tourist visa ", "Student visa ", "Official visa", "Free visa", "Medical visa"];
+
 
     return (
         <div>
@@ -22,56 +40,69 @@ const AllVisa = () => {
                 <p className='md:pb-10 md:px-56 text-xs px-24 pb-6 md:text-lg'>
                     Here are all the visa Available here . Chose your dream visa here. And visit your dream country.
                 </p>
+
+                <div className="pb-4 flex items-center justify-center">
+                    <h1 className='mx-2 text-xl font-bold'>  Filter Visa: </h1>
+                    <select name="visaType" onChange={HandleFilter} className="mt-1 bg-transparent border border-gray-300 shadow-md py-2 px-5">
+                        <option value="x">Select a visa type to filter</option>
+                        {visaTypes.map((type, i) => (<option key={i} value={type}>{type}</option>))} </select>
+                </div>
             </div>
 
 
             {/* --------------------------all visa--------------------- */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-10 md:mx-10 my-10">
+            {AllVisa.length == 0 ? <div> <h1 className='text-4xl font-mono font-black text-center my-10'>No Visa Available</h1> </div> :
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-10 md:mx-10 my-10">
+                    {
+                        AllVisa.map(visa =>
+                            <div key={visa._id}>
 
-                {AllVisa.map(visa =>
-                    <JackInTheBox>
-                        <div
-                            className=" shadow-md my-4 gap-5 mx-2 py-8 border rounded-md bg-[#FCDC2A]"
-                            key={visa._id}>
+                                <div
+                                    className=" shadow-md my-4 gap-5 mx-2 py-8 border rounded-md bg-[#FCDC2A]"
+                                >
+                                    <JackInTheBox>
+                                        <div className="w-3/4 mx-auto h-[200px] md:h-[250px] lg:h-[150px]">
+                                            <img src={visa.countryImage} className="w-full h-full object-cover rounded-xl shadow-xl border mx-auto border-black" />
+                                        </div>
+                                    </JackInTheBox>
+                                    <JackInTheBox>
+                                        <div className="">
 
-                            <div className="w-3/4 mx-auto h-[200px] md:h-[250px] lg:h-[150px]">
-                                <img src={visa.countryImage} className="w-full h-full object-cover rounded-xl shadow-xl border mx-auto border-black" />
-                            </div>
+                                            {/* ----------------------text-------------------------- */}
+                                            <div className='md:py-5 text-center'>
 
-                            <div className="">
+                                                <p className='bg-white text-xl font-serif italic font-semibold mb-2 w-full'> {visa.countryName}</p>
 
-                                {/* ----------------------text-------------------------- */}
-                                <div className='md:py-5 text-center'>
+                                                <h1 className=" font-bold">Visa Type : {visa.visaType}</h1>
 
-                                    <p className='bg-white text-xl font-serif italic font-semibold mb-2 w-full'> {visa.countryName}</p>
+                                                <p className=" py-3 ">
+                                                    Visa Fee: {visa.fee ? visa.fee : 300} $
+                                                </p>
+                                                <p className=" ">
+                                                    Visa Validity: {visa.validity ? visa.validity : "5 years"}
+                                                </p>
 
-                                    <h1 className=" font-bold">Visa Type : {visa.visaType}</h1>
+                                            </div>
 
-                                    <p className=" py-3 ">
-                                        Visa Fee: {visa.fee ? visa.fee : 300} $
-                                    </p>
-                                    <p className=" ">
-                                        Visa Validity: {visa.validity ? visa.validity : "5 years"}
-                                    </p>
 
+                                            {/* --------------buttons---------------------- */}
+                                            <div className="flex items-center bg-white py-2 justify-center gap-5">
+
+                                                {/* ---details */}
+                                                <Link to={`/allVisa/${visa._id}`}>
+                                                    <button className="btn btn-sm btn-outline">
+                                                        Details
+                                                    </button></Link>
+                                            </div>
+
+                                        </div>
+                                    </JackInTheBox>
                                 </div>
 
+                            </div>)
+                    }
 
-                                {/* --------------buttons---------------------- */}
-                                <div className="flex items-center bg-white py-2 justify-center gap-5">
-
-                                    {/* ---details */}
-                                    <Link to={`/allVisa/${visa._id}`}>
-                                        <button className="btn btn-sm btn-outline">
-                                            Details
-                                        </button></Link>
-                                </div>
-
-                            </div>
-
-                        </div></JackInTheBox>)}
-
-            </div>
+                </div>}
         </div>
     );
 };
